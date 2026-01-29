a25fd205ba1c0a99b7c58da871051ebb17774c0c
Yoav0akr <carlosyoav08@gmail.com>
2026-01-28 19:59:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//tapar con el parche
+const btn = document.getElementById("btn");
+    const parche = document.querySelector(".parche");
+    //al hacer click en el boton
+    btn.addEventListener("click", () => {
+        //alternar la clase no-ver en el parche
+        parche.classList.toggle("no-ver");
+    btn.textContent = parche.classList.contains("no-ver") ? "Ver menos" : "Ver mas";
+    });
+
+//enviar a mongoDB
+      document.getElementById("correo").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const datos = Object.fromEntries(new FormData(e.target));
+    const res = await fetch("./API/mongoDB", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(datos),
+    });
+    const json = await res.json();
+    console.log(json);
+    alert("Gracias por suscribirte a nuestro sistema para recibir menos atencion al cliente. Pronto recibirás noticias nuestras.");
+    e.target.reset();
+  });
